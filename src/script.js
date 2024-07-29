589f3fd1be8715283ff07f798eb0e6e5ca4dbeb7
Maciej Staniec <maciej.staniec@yahoo.co.uk>
2024-07-29 15:17:57 +0100

    commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+if (document.querySelector("#data")) {
+  window.addEventListener("resize", updateText);
+  updateText();
+
+  function updateText() {
+    const data = document.querySelector("#data");
+    data.textContent = `Width: ${window.innerWidth}, Ratio: ${window.devicePixelRatio}`;
+  }
+}
