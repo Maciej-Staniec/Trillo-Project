136b83f003cb985f4c211f44944791828c005060
Maciej Staniec <maciej.staniec@yahoo.co.uk>
2024-08-02 19:16:21 +0100

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,32 @@
-const gulp = require("gulp");
-const concat = require("gulp-concat");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
-const htmlmin = require("gulp-html-minifier-terser");
-const uglify = require("gulp-uglify");
+// Require the necessary Gulp plugins
+const gulp = require("gulp"); // Gulp core library
+const concat = require("gulp-concat"); // Concatenates multiple files into one
+const postcss = require("gulp-postcss"); // Processes CSS with PostCSS plugins (autoprefixer and cssnano)
+const autoprefixer = require("autoprefixer"); // Adds vendor prefixes to CSS rules
+const cssnano = require("cssnano"); // Minifies CSS files
+const uglify = require("gulp-uglify"); // Minifies JavaScript files
 
+// Define a Gulp task named 'css' for processing CSS files
 gulp.task("css", function () {
+  // Define an array of PostCSS plugins to use
   const plugins = [autoprefixer(), cssnano()];
+  // Return a stream that:
   return gulp
-    .src("src/styles/*.css") // Path to CSS files in the 'dist/styles' directory.
-    .pipe(concat("main.compiled.css")) // Concatenate CSS files into one file.
-    .pipe(postcss(plugins)) // Process the CSS file using PostCSS.
-    .pipe(gulp.dest("dist/styles")); // Save the outcome file in the 'dist/styles' directory
+    .src("src/styles/*.css") // 1. Reads all CSS files from the 'src/styles' directory
+    .pipe(concat("main.compiled.css")) // 2. Concatenates them into a single file named 'main.compiled.css'
+    .pipe(postcss(plugins)) // 3. Processes the concatenated file using PostCSS plugins (autoprefixer and cssnano)
+    .pipe(gulp.dest("dist/styles")); // 4. Saves the resulting file in the 'dist/styles' directory
 });
 
+// Define a Gulp task named 'js' for processing JavaScript files
 gulp.task("js", function () {
+  // Return a stream that:
   return gulp
-    .src("src/*.js")
-    .pipe(concat("script.compiled.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest("dist"));
+    .src("src/*.js") // 1. Reads all JavaScript files from the 'src' directory
+    .pipe(concat("script.compiled.js")) // 2. Concatenates them into a single file named 'script.compiled.js'
+    .pipe(uglify()) // 3. Minifies the concatenated file using Uglify
+    .pipe(gulp.dest("dist")); // 4. Saves the resulting file in the 'dist' directory
 });
 
-// Run 2 tasks subsequently
+// Define a Gulp task named 'build' to run the 'css' and 'js' tasks in series
 gulp.task("build", gulp.series("css", "js"));
